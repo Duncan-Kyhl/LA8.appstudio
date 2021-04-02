eb3f9f0a57ff196f1a3569f1585397c9082c5c06
Duncan-Kyhl <Duncan-Kyhl@users.noreply.github.com>
2021-04-02 14:54:45 -0500

    finish

--- LA8DuncanK.appstudio/forms/favExercises/favExercises.js
+++ LA8DuncanK.appstudio/forms/favExercises/favExercises.js
@@ -1,13 +1,34 @@
 
-var exercises = ["pullups","pushups","situps","jogging","plank","curls"]
+var exercises = ["pullups","pushups","jogging","curls"]
 var coreExercises = ["situps","plank"]
+var selection = []
 
 favExercises.onshow=function(){
-    favExercises.clear()   
+    slctExercise.clear()
     for (i=0; i<exercises.length; i++) {
-      exercises.addItem(exercises[i])
+      slctExercise.addItem(exercises[i])
       }
-    for (i=0; i<core.length; i++) {
-      exercises.addItem(exercises[i])
+    for (i=0; i<coreExercises.length; i++) {
+      slctExercise.addItem(coreExercises[i])
       }
 }
+
+btnShowResults.onclick=function(){
+  for (i=0; i<slctExercise.text.length; i++) {
+    selection.push(slctExercise.text[i])
+    }
+  console.log(selection)
+  if (selection[0]=="situps" && selection[1]=="plank") {
+      lblMessage3.value == (`You chose ${selection[0]} and ${selection[1]} - those are the two core exercises.`)
+    } else if (selection[0]=="plank" && selection[1]=="situps"){
+      lblMessage3.value == (`You chose ${selection[0]} and ${selection[1]} - those are the two core exercises.`)
+    } else {
+      lblMessage3.value == ("You did not pick the two core exercises.")
+      }
+}
+
+btnNextForm3.onclick=function(){
+  ChangeForm(mobileNav)
+}
+
+
